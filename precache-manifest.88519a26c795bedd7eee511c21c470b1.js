54ea242ea8152f9d276df8523fff9b8bb6959a32
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-11 14:51:07 +0200

    Updates

---
+++ precache-manifest.88519a26c795bedd7eee511c21c470b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222c7642cf683d456dc0a5b3a00dbd2b",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "598296bf8023db7b1461",
+    "url": "/react_add-todo-form/static/css/main.33b67bc8.chunk.css"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/react_add-todo-form/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "598296bf8023db7b1461",
+    "url": "/react_add-todo-form/static/js/main.49824fdd.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
